fab4f5dbb1ab3a47c603f01f5332895a933ec90a
wnsgudchl0302 <wnsgudchl0302@naver.com>
2022-03-31 00:06:00 +0900

    Create CollegeMate Backend-Project

---
+++ src/main/java/com/hanium/collegemate/CollegeMateApplication.java
@@ -0,0 +1,13 @@
+package com.hanium.collegemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollegeMateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hanium/collegemate/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.hanium.collegemate.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @PostMapping("/test")
+    public String test(){
+        return "Test Complete";
+    }
+}
